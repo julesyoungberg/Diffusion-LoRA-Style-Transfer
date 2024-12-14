3fc7b4b097b6e43a1c7ef902337a22d399c6f129
trapoom555 <trapoom555@gmail.com>
2024-12-14 23:51:54 +0800

    update infer config

--- infer_folder.sh
+++ infer_folder.sh
@@ -2,7 +2,7 @@
 
 export PYTHONPATH="$PYTHONPATH:$pwd"
 
-OUT_FOLDER="out_monet_all_bs32"
+OUT_FOLDER="out_monet_all"
 
 if [ ! -d "$OUT_FOLDER" ]; then
     mkdir -p "$OUT_FOLDER"
@@ -10,7 +10,7 @@ fi
 
 python inference/inference_folder.py \
 --pretrained_path "/home/trapoom555/data/hf_models/stable-diffusion-2-1" \
---lora_path "/home/trapoom555/code/Diffusion-LoRA-Style-Transfer/lora_ckpt/monet_all_bs_32" \
+--lora_path "/home/trapoom555/code/Diffusion-LoRA-Style-Transfer/lora_ckpt/monet_all" \
 --folder_path "/home/trapoom555/data/monet_data/photo_jpg/" \
---strength 0.3 \
+--strength 0.2 \
 --save_path "$OUT_FOLDER"
