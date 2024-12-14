ac7fa546774d596a9379c9213d6d6d787787c70b
trapoom555 <trapoom555@gmail.com>
2024-12-14 15:04:37 +0800

    update inference config

--- infer_img.sh
+++ infer_img.sh
@@ -4,7 +4,7 @@ export PYTHONPATH="$PYTHONPATH:$pwd"
 
 python inference/transfer_style.py \
 --pretrained_path "/home/trapoom555/data/hf_models/stable-diffusion-2-1" \
---lora_path "/home/trapoom555/code/Diffusion-LoRA-Style-Transfer/lora_ckpt/300_steps" \
---image_path "/home/trapoom555/code/Diffusion-LoRA-Style-Transfer/ting.JPG" \
+--lora_path "/home/trapoom555/code/Diffusion-LoRA-Style-Transfer/lora_ckpt/monet_300" \
+--image_path "/home/trapoom555/data/monet_data/photo_jpg/4b497dc591.jpg" \
 --strength 0.3 \
 --save_path "styled_image.jpg"
