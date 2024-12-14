91b24af14a63b9742b8ceb3f35c266dbe4041fd2
trapoom555 <trapoom555@gmail.com>
2024-12-14 23:52:06 +0800

    update train config

--- train.sh
+++ train.sh
@@ -9,9 +9,9 @@ python train/trainer.py \
 --lora_r 8 \
 --lora_alpha 16 \
 --lora_dropout 0.1 \
---train_strength 0.3 \
+--train_strength 1.0 \
 --learning_rate 0.00001 \
 --batch_size 8 \
 --grad_accumulation 4 \
---max_epochs 50 \
---save_name "monet_all_bs_32"
+--max_epochs 100 \
+--save_name "monet_all"
